41e807a70fbec6f2df1018f08bc04a023c2a2a2c
yankeealpha444-arch <yankeealpha444@gmail.com>
2026-04-28 11:48:11 +1000

    v28 force clipper on root route

--- vision-clip-hub/src/App.tsx
+++ vision-clip-hub/src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Upload from "./pages/Upload.tsx";
-import Index from "./pages/Index.tsx";
 import LinkClipperMvp from "./pages/LinkClipperMvp.tsx";
 import Preview from "./pages/Preview.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
@@ -19,9 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Upload />} />
+          <Route path="/" element={<LinkClipperMvp />} />
           <Route path="/link-clipper" element={<LinkClipperMvp />} />
-          <Route path="/clips" element={<Index />} />
+          <Route path="/clips" element={<LinkClipperMvp />} />
           <Route path="/preview" element={<Preview />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/dashboard/:projectId" element={<Dashboard />} />
